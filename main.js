91f1266390e7e55c852fa17240515604591bda98
andbreak <1622170503@qq.com>
2024-06-15 13:02:43 +0800

    提交信息

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow, ipcMain } = require("electron/main");
 const path = require("node:path");
 
 const createWindow = () => {
@@ -15,6 +15,8 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
+
   createWindow();
 
   app.on("activate", () => {
